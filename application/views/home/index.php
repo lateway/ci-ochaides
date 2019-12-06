517f9560a3ffbd409d6a70dc5a7d860ae989c8bb
@Oci_tz <co.jingga@gmail.com>
2019-12-07 04:31:29 +0700

    create information menu center

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -15,16 +15,19 @@
 	</div>
 	<div class="htab">
 		<div class="item1">
-			<p>Follow me</p>
+			<h3>Follow me</h3>
 			<img src="<?= base_url('assets/img/bg/ig.png'); ?>" alt="myIG">
+			<a href="#">@oci_tz</a>
 		</div>
 		<div class="item2">
-			<p>Subscribe my Channel</p>
+			<h3>Subscribe</h3>
 			<img src="<?= base_url('assets/img/bg/yt.png'); ?>" alt="myIG">
+			<a href="#">@oci_tz</a>
 		</div>
 		<div class="item3">
-			<p>Invited me</p>
+			<h3>Invited me</h3>
 			<img src="<?= base_url('assets/img/bg/fb.png'); ?>" alt="myIG">
+			<a href="#">@oci_tz</a>
 		</div>
 	</div>
 
@@ -32,46 +35,38 @@
 
 	<!-- wrap begin :: DONT REMOVE OR DELETE-->
 	<div class="ct">
+		<h3>Welcome to the jungle !</h3>
+		<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit nostrum nesciunt dicta officia, amet illum molestias incidunt assumenda eum id perferendis quaerat quasi, totam, magnam quo sint a accusantium temporibus?</p>
 		<!-- center bar -->
-		<div class="box">
-			<div class="card">
-				<div class="card-content">
-					<h2>01</h2>
-					<h3>service</h3>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloribus quas veritatis blanditiis rerum qui.</p>
-					<a href="#">more</a>
-				</div>
+
+
+		<div class="info">
+			<div class="info1">
+				<i class="material-icons">code</i>
+				Lorem ipsum dolor sit amet, idque mollis consetetur at vis, eam at sale atqui. Sapientem qualisque reprehendunt eu his, ne sit oratio accusam. In has ocurreret vulputate, etiam sadipscing ut per. 1
 			</div>
-			<!-- <div class="card">
-				<div class="card-content">
-					<h2>02</h2>
-					<h3>service</h3>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloribus quas veritatis blanditiis rerum qui.</p>
-					<a href="#">more</a>
-				</div>
+			<div class="info2">
+				Lorem ipsum dolor sit amet, idque mollis consetetur at vis, eam at sale atqui. Sapientem qualisque reprehendunt eu his, ne sit oratio accusam. In has ocurreret vulputate, etiam sadipscing ut per. 2
+				<i class="material-icons">code</i>
 			</div>
-			<div class="card">
-				<div class="card-content">
-					<h2>03</h2>
-					<h3>service</h3>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloribus quas veritatis blanditiis rerum qui.</p>
-					<a href="#">more</a>
-				</div>
+			<div class="info3">
+				<i class="material-icons">code</i>
+				Lorem ipsum dolor sit amet, idque mollis consetetur at vis, eam at sale atqui. Sapientem qualisque reprehendunt eu his, ne sit oratio accusam. In has ocurreret vulputate, etiam sadipscing ut per. 3
 			</div>
-			<div class="card">
-				<div class="card-content">
-					<h2>04</h2>
-					<h3>service</h3>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloribus quas veritatis blanditiis rerum qui.</p>
-					<a href="#">more</a>
-				</div>
-			</div> -->
+			<div class="info4">
+				Lorem ipsum dolor sit amet, idque mollis consetetur at vis, eam at sale atqui. Sapientem qualisque reprehendunt eu his, ne sit oratio accusam. In has ocurreret vulputate, etiam sadipscing ut per. 4
+				<i class="material-icons">code</i>
+			</div>
+			<div class="info5">
+				<i class="material-icons">code</i>
+				Lorem ipsum dolor sit amet, idque mollis consetetur at vis, eam at sale atqui. Sapientem qualisque reprehendunt eu his, ne sit oratio accusam. In has ocurreret vulputate, etiam sadipscing ut per. 5
+			</div>
+			<div class="info6">
+				Lorem ipsum dolor sit amet, idque mollis consetetur at vis, eam at sale atqui. Sapientem qualisque reprehendunt eu his, ne sit oratio accusam. In has ocurreret vulputate, etiam sadipscing ut per. 6
+				<i class="material-icons">code</i>
+			</div>
 		</div>
-		<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, ratione perferendis, quidem, consequuntur nemo voluptatem maxime cupiditate enim consectetur fugit natus voluptatum autem nisi dolor facere et atque deleniti libero.</p> <br><br>
 
-		<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, ratione perferendis, quidem, consequuntur nemo voluptatem maxime cupiditate enim consectetur fugit natus voluptatum autem nisi dolor facere et atque deleniti libero.</p> <br><br>
-
-		<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, ratione perferendis, quidem, consequuntur nemo voluptatem maxime cupiditate enim consectetur fugit natus voluptatum autem nisi dolor facere et atque deleniti libero.</p> <br><br>
 
 	</div>
 
